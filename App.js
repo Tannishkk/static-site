f2d803de88995fc85153d23e169583d1d6b5ef76
Tannishkk <tanishk.saxena_cs23@gla.ac.in>
2025-07-18 23:54:37 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h1>🚀 Hello from React + AWS!</h1>
+      <p>This site was deployed automatically via GitHub → CodeBuild → S3!</p>
+    </div>
+  );
+}
+
+export default App;
